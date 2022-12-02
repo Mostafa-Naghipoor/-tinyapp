c965e9a19e02836973c1a4f3f1129fb205190a49
Mostafa-Naghipoor <mostafa.naghipoor@hotmail.com>
2022-12-01 21:30:56 -0800

    modified

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ const PORT = 8080; // default port 8080
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
+const methodOverride = require('method-override')
 const { checkIfUserExists, findUser, returnUsersUrls, generateRandomString } = require('./helper.js');
 app.use(express.urlencoded({ extended: true }));
 
@@ -13,6 +14,11 @@ app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2'],
 }));
+
+app.use(methodOverride('_method'))
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
 
 const urlDatabase = {
   b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
